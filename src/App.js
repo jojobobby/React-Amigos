78a7809ddf3a6154a7d607018f0b1ceaa1c7d7c5
Angelo Flores <aflores@aflores-ltm.vailsys.deerfield>
2023-04-04 14:36:17 -0500

    Js for everyones pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [showPage, setShowPage] = useState(false);
+
+  const togglePage = () => {
+    setShowPage(!showPage);
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +16,20 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <button onClick={togglePage}>
+          {showPage ? 'Hide Page' : 'Show Page'}
+        </button>
+
+        {/* Conditionally render the page content based on the value of 'showPage' */}
+        {showPage && (
+          <div>
+            <h1>Welcome to My Website</h1>
+            <p>This is the homepage of my website.</p>
+            <p>Feel free to browse around!</p>
+          </div>
+        )}
+
         <a
           className="App-link"
           href="https://reactjs.org"
