57b55e1dd1630cce89b0fb4ff7072b77b0c01e11
Estrella Anguiano-Valdez <eanguiano@eanguiano-ltm.vailsys.deerfield>
2023-04-06 13:14:21 -0500

    Define Array and Object

--- src/EstrellasPage.js
+++ src/EstrellasPage.js
@@ -1,27 +1,66 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 
-function EstrellaPage() {
+function Board() {
   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Estrella Page
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='board'>
+        <h1 className='title'>Tic Tac Toe</h1>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
 
-export default EstrellaPage;
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+      <Array />
+      <Object />
+
+    </div>
+  )
+
+  }
+
+  function Array(){
+    const candy = ['starburst', 'skittes', 'm&m', 'WarHeads'];
+    console.log(candy)
+  }
+
+  function Object () {
+    const colors ={
+      Sammy : "beige",
+      Estrella : "Purple",
+      Liz: "red"
+    }
+
+    console.log(colors);
+  }
+
+  function Square (){
+
+    const [value, setValue] = useState(null);
+
+    function handleClick(){
+      setValue('X');
+    }
+
+    return(
+        <button className='square' onClick={handleClick}>{value}</button>
+    )
+  }
+
+
+
+export default Board;
