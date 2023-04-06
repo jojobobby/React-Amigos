b5b7d58ed12fec278f04c481becb836dcc0813b1
Lizbeth Villanueva <lvillanueva@lvillanueva-ltm.vailsys.deerfield>
2023-04-06 15:21:43 -0500

    has objects and added more links

--- src/LizsPage.js
+++ src/LizsPage.js
@@ -1,19 +1,53 @@
 import './LizsPage.css';
 import './App.css';
-
+import logo from './Liz-logo.png';
+import donate from './Liz-Donate.png';
 
 function LizPage() {
-  const adoptablePets = ["Ripley", "Sol", "Omar", "Heinz", "Tyrone", "Ford", "Zamara", "Darwin"];
+  const adoptablePets = [
+    {
+      name: "Ripley",
+      type: "Dog"
+    },
+    {
+      name: "Sol",
+      type: "Cat"
+    },
+    {
+      name: "Heinz",
+      type: "Cat"
+    },
+    {
+      name: "Tyrone",
+      type: "Dog"
+    },
+    {
+      name: "Ford",
+      type: "Dog"
+    },
+    {
+      name: "Zamara",
+      type: "Dog"
+    },
+    {
+      name: "Darwin",
+      type: "Dog"
+    }
+    
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <img src= 'https://picsum.photos/id/237/200' className="App-logo" alt="logo" />
+        <img src= {logo} className="App-logo" alt="logo" width="150" height="150"/>
+        <a href="https://www.pawschicago.org/how-to-help/donate"><img src={donate} alt="Donate Now!"></img></a>
+        <h2>Services: <a href="https://www.pawschicago.org/our-work/pets-adoption/pets-available">Pet Adoption</a>, <a href="https://www.pawschicago.org/how-to-help/foster">Fostering</a>, <a href="https://www.pawschicago.org/how-to-help/volunteer">Volunteering</a>.</h2>
         <p>
         Meet adoptable pets and complete the application process. Help us find homes for Chicago's homeless pets! 
-        Services: Pet Adoption, Fostering, Volunteering.
         </p>
         {adoptablePets.map(pet => (
-           <h1> {pet} </h1>
+          <div>
+           <h3> {`Name: ${pet.name} Type: ${pet.type}`}</h3>
+          </div>
         ))}
         <a
           className="App-link"
@@ -21,7 +55,7 @@ function LizPage() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Apdopt Me!
+          Adopt Me!
         </a>
       </header>
     </div>
