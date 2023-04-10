1c2adfc70d3094369a81abfa482fca3ad243822b
Ben <brubio@vailsys.com>
2023-04-10 11:50:12 -0500

    ttt game

--- src/BensPage.js
+++ src/BensPage.js
@@ -1,13 +1,15 @@
-import './App.css';
 import React, { useState } from 'react';
+import './App.css';
+import Board from './bensFiles/ben-tttgame.js';
 
 function BenPage() {
   return (
     <div className="App">
       <header className="App-header">
-        <p className="ben-title"> Ben's Page </p>
-      </header>
-        <BenButton />
+      <p className="ben-title"> Ben's Page </p>
+      </header> 
+      <BenButton />
+      <Board />
     </div>
   );
 }
