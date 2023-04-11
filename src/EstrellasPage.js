28a5ad1eea5ebe74f58ee8b3c048e1b74c90a4f1
Starsii <estrellaanguiano99@gmail.com>
2023-04-11 11:54:47 -0500

    Working with Props

--- src/EstrellasPage.js
+++ src/EstrellasPage.js
@@ -1,22 +1,86 @@
-// import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react';
+import './EstrellaPage.css';
 
-function Board() {
-  const candy = ['starburst', 'skittes', 'm&m', 'WarHeads'];
-
-  const colors ={
-    Sammy : "beige",
-    Estrella : "Purple",
-    Liz: "red"
-  };
-
+function EstrellaPage() {
+  
   return (
-    console.log(colors),
-    console.log(candy)
+    <div>
+      <Header />
+      <Hero />
+
+      <div className='card-row'>
+        <Card 
+          img = "./bean.webp"
+          rating= "4.0"
+          title = "Bean"
+        />
+        <Card 
+          img = "./chiSkyline.jpeg"
+          rating= "5.0"
+          title = "SKyLine View"
+        />
+        <Card 
+          img = "./IceCream.jpeg"
+          rating = "3.8"
+          title = "Ice Cream Museum"
+        />
+      </div>
+      {/* <Footer /> */}
+    </div>
   );
-  };
+}
+
+function Header(){
+    return (
+      <header>
+        <nav className="nav">
+          <img src= "./airbnbLogo.png" className='airbnb-Logo' />
+        </nav>
+      </header>
+    )
+  }
+
+function Hero(){
+    return (
+      <div className='Hero-Content'>
+          <img src='./chiSkyline.jpeg' className='chi-Skyline' />
+
+          <h1 className='hero-header'>Online Experince</h1>
+
+          <p className='hero-paragraph'>
+            Lorem ipsum dolor sit amet. Et adipisci Quis est rerum 
+            expedita hic quod maiores eos nostrum labore. Hic adipisci 
+            similique et dolorem quaerat sit asperiores numquam qui 
+            magnam consequatur!
+          </p>
+
+      </div>
+    )
+}
+
+function Card({img, rating, title}){
+    return(
+      <div class='row'>
+         <div>
+            <img src={img} className='card-Skyline' />
+              <div>
+                <span><img src='./pinkStar.webp' className='star'/></span>
+                <span>{rating}</span>
+                  <div>
+                    <span>{title}</span>
+                  </div>
+              </div>
+         </div>
+      </div>
+    )
+}
 
 
+function Footer(){
+    return (
+      <footer className='footer'>
+          <small>@2023 Anguiano developement. All rights reserved.</small>
+      </footer>
+    )
+  }
 
-export default Board;
+export default EstrellaPage;
