c8f32ab44dd813beb778ecf2416b98a406798d45
Starsii <estrellaanguiano99@gmail.com>
2023-04-14 10:18:04 -0500

    airBnB Mock

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export default [
+        {
+          img: "./estrellaImages/bean.webp",
+          rating: "4.0",
+          title : "Bean",
+        //   openSpots: "0"
+        },
+        {
+          img: "./estrellaImages/chiSkyline.jpeg",
+          rating: "5.0",
+          title: "SKyLine View"
+        },
+        {
+          img: "./estrellaImages/IceCream.jpeg",
+          rating: "3.8",
+          title: "Ice Cream Museum"
+        },
+        {
+            img: "./estrellaImages/IceCream.jpeg",
+            rating: "3.8",
+            title: "Ice Cream Museum"
+        },
+        {
+            img: "./estrellaImages/IceCream.jpeg",
+            rating: "3.8",
+            title: "Ice Cream Museum"
+        },
+        {
+            img: "./estrellaImages/Cta.jpeg",
+            rating: "0.0",
+            title: "A Day In The Life Tour"
+        }
+        
+]
